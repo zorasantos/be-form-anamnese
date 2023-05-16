35a8403f6929524ec02708a87bb9b742f04b7bdb
zorasantos <zoranildo22@gmail.com>
2023-05-16 07:31:23 -0300

    config: complete unit and integration test

--- src/application/modules/PersonalData/usecase/CreatePersonalData/PersonalData.e2e.spec.ts
+++ src/application/modules/PersonalData/usecase/CreatePersonalData/PersonalData.e2e.spec.ts
@@ -1,22 +1,6 @@
-import 'dotenv/config'
-import {
-  createAppSupertestAdapter,
-  appServer,
-} from '@infra/adapters/supertestAdapter'
-// import {
-//   createExpressAdapter,
-//   createFastifyAdapter,
-// } from '@infra/adapters/appAdapter'
+import request from 'supertest'
 
-import { app as expressApp } from '@infra/ports/express'
-// import { app as fastifyApp } from '@infra/ports/fastify'
-
-// const serverType = process.env.SERVER_TYPE || 'express'
-
-// const adapter =
-//   serverType === 'fastify'
-//     ? createFastifyAdapter(fastifyApp)
-//     : createExpressAdapter(expressApp)
+import { app as fastifyApp } from '@infra/ports/fastify'
 
 const data = {
   name: 'Zora',
@@ -35,24 +19,17 @@ const data = {
 }
 
 describe('Personal data', () => {
-  let closeServer: any
-  beforeEach(async () => {
-    const server = expressApp.listen(3001, () => {
-      console.log(`Express server Running in port ${3001}`)
-    })
-
-    closeServer = server
+  beforeEach(() => {
+    fastifyApp.listen()
   })
 
-  afterEach(async () => {
-    await closeServer.close()
+  afterEach(() => {
+    fastifyApp.close()
   })
-
   test('Create Personal Data', async () => {
     // const response = await request(app).post('/form/personal').send(data)
-    console.log('appServer', appServer)
 
-    const response = await createAppSupertestAdapter()
+    const response = await request(fastifyApp.server)
       .post('/form/personal')
       .send(data)
     expect(response.status).toBe(201)
